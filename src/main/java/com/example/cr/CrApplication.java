4df920d622239b883a7518483c6d96aa08469a6f
zxhhhxz1221 <1305894626@qq.com>
2025-03-02 09:20:44 +0800

    Init

---
+++ src/main/java/com/example/cr/CrApplication.java
@@ -0,0 +1,13 @@
+package com.example.cr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrApplication.class, args);
+    }
+
+}
