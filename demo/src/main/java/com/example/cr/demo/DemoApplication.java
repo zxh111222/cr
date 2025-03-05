0591ed30758cc78f88c327ce00e936671b078aeb
zxhhhxz1221 <1305894626@qq.com>
2025-03-05 20:14:32 +0800

    fix(user|demo): 主动添加 @ComponentScan("com.example.cr")注解,覆盖Spring Boot 默认的扫描规则
    默认的扫描规则为：扫描启动类所在的包

--- demo/src/main/java/com/example/cr/demo/DemoApplication.java
+++ demo/src/main/java/com/example/cr/demo/DemoApplication.java
@@ -3,8 +3,10 @@ package com.example.cr.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.cr")
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
--- user/src/main/java/com/example/cr/user/UserApplication.java
+++ user/src/main/java/com/example/cr/user/UserApplication.java
@@ -3,8 +3,10 @@ package com.example.cr.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.cr")
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
