30eb9d3577a78c8239c0823e733d02fe37498c02
zxhhhxz1221 <1305894626@qq.com>
2025-03-07 19:38:42 +0800

    feat(user): 用 MyBatis 代码生成器插件自动生成 User 持久层相关代码(error)

---
+++ user/src/main/java/com/example/cr/user/entity/User.java
@@ -0,0 +1,92 @@
+package com.example.cr.user.entity;
+
+import java.util.Date;
+
+public class User {
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String mobile;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", email=").append(email);
+        sb.append(", password=").append(password);
+        sb.append(", mobile=").append(mobile);
+        sb.append(", createdAt=").append(createdAt);
+        sb.append(", updatedAt=").append(updatedAt);
+        sb.append("]");
+        return sb.toString();
+    }
+}
--- user/src/main/java/com/example/cr/user/mapper/UserMapper.java
+++ user/src/main/java/com/example/cr/user/mapper/UserMapper.java
@@ -1,6 +1,30 @@
 package com.example.cr.user.mapper;
 
+import com.example.cr.user.entity.User;
+import com.example.cr.user.entity.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper {
-    long count();
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(User row);
+
+    int insertSelective(User row);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("row") User row, @Param("example") UserExample example);
+
+    int updateByExample(@Param("row") User row, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User row);
+
+    int updateByPrimaryKey(User row);
 }
