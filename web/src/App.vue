91115ceeb92f4f2feb7279def3b0207084c12544
zxhhhxz1221 <1305894626@qq.com>
2025-03-09 15:47:22 +0800

    refactor(web): App.vue 去除头部的导航栏，只保留最简洁的 <RouterView />

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,25 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
 <style scoped>
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
+
 </style>
