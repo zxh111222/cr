a8f2bef9663ada778450bf23ff06ecb0bb897642
zxhhhxz1221 <1305894626@qq.com>
2025-03-09 11:06:53 +0800

    feat(user): 增加校验：手机号必须是11位

--- user/src/main/java/com/example/cr/user/request/UserRequest.java
+++ user/src/main/java/com/example/cr/user/request/UserRequest.java
@@ -1,10 +1,12 @@
 package com.example.cr.user.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class UserRequest {
 
     @NotBlank
+    @Size(min = 11, max = 11, message = "手机号必须是11位")
     String mobile;
 
     public String getMobile() {
