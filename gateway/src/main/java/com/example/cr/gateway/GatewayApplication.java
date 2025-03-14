d015fd3fe84bc26b5499c17a13186d0654422b12
zxhhhxz1221 <1305894626@qq.com>
2025-03-14 21:30:44 +0800

    fix(gateway): 复习之前的 @Import 注解，精准注入 BearerTokenFilter 需要用到的 CustomJWTUtils

--- gateway/src/main/java/com/example/cr/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/cr/gateway/GatewayApplication.java
@@ -1,11 +1,13 @@
 package com.example.cr.gateway;
 
+import com.example.cr.common.util.CustomJWTUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@ComponentScan("com.example.cr")
+//@ComponentScan("com.example.cr")
+@Import(CustomJWTUtils.class)
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
