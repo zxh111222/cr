4d7bae6a64b91736f8a7cb7d1013fe07ddd8722f
zxhhhxz1221 <1305894626@qq.com>
2025-03-19 21:30:50 +0800

    feat(generator): 读取上一步获取到的配置文件，解析出表名和对应的实体名

--- generator/src/main/java/com/example/cr/generator/CodeGenerator.java
+++ generator/src/main/java/com/example/cr/generator/CodeGenerator.java
@@ -8,6 +8,7 @@ import org.dom4j.io.SAXReader;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CodeGenerator {
@@ -20,7 +21,7 @@ public class CodeGenerator {
     }
 
     public static void main(String[] args) throws Exception {
-//        CustomFreemarkerUtil.getTemplate("test.ftl");
+        CustomFreemarkerUtil.getTemplate("test.ftl");
 //
 //        String tableName = "user";
 //        String c = tableName.charAt(0) + "";
@@ -33,11 +34,26 @@ public class CodeGenerator {
 //        String className = String.format("%sService", tableNameUpperCase);
 //        System.out.println(className);
 //
-//        Map<String, Object> data = new HashMap<>();
-//        data.put("className", className);
 //
-//        CustomFreemarkerUtil.generate(toPath + className + ".java", data);
-        readConfigurationFileFromPomXml();
+
+        String configurationFile = readConfigurationFileFromPomXml();
+        Document document = new SAXReader().read("generator/" + configurationFile);
+        List<Node> nodes = document.selectNodes("//table");
+        for (Node table : nodes) {
+            Node tableNameNode = table.selectSingleNode("@tableName");
+            Node domainObjectNameNode = table.selectSingleNode("@domainObjectName");
+            String tableName = tableNameNode.getText();
+            String domainObjectName = domainObjectNameNode.getText();
+            System.out.println("表名: " + tableName + ",对应的实体名: " + domainObjectName);
+
+            String className = String.format("%sService", domainObjectName);
+
+            Map<String, Object> data = new HashMap<>();
+            data.put("className", className);
+
+            CustomFreemarkerUtil.generate(toPath + className + ".java", data);
+        }
+
     }
 
     private static String readConfigurationFileFromPomXml() throws DocumentException {
--- generator/src/main/java/com/example/cr/generator/demo/UserService.java
+++
@@ -1,4 +0,0 @@
-package com.example.cr.generator.demo;
-
-public class UserService {
-}
