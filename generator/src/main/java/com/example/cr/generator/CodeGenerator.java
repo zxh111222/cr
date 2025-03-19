81c29a5f106b97981f89e72d91354ecbbe612573
zxhhhxz1221 <1305894626@qq.com>
2025-03-19 21:18:51 +0800

    feat(generator): 从 pom.xml 读取 mybatis-generator 需要用到的持久层配置文件

--- generator/src/main/java/com/example/cr/generator/CodeGenerator.java
+++ generator/src/main/java/com/example/cr/generator/CodeGenerator.java
@@ -1,6 +1,11 @@
 package com.example.cr.generator;
 
 import com.example.cr.generator.util.CustomFreemarkerUtil;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
+
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,27 +13,42 @@ import java.util.Map;
 public class CodeGenerator {
     static String toPath = "generator/src/main/java/com/example/cr/generator/demo/";
 
+    static String pomPath ="generator/pom.xml";
+
     static {
         new File(toPath).mkdirs();
     }
 
     public static void main(String[] args) throws Exception {
-        CustomFreemarkerUtil.getTemplate("test.ftl");
+//        CustomFreemarkerUtil.getTemplate("test.ftl");
+//
+//        String tableName = "user";
+//        String c = tableName.charAt(0) + "";
+//        String upperCase = c.toUpperCase();
+//        System.out.println(upperCase);
+//        String substring = tableName.substring(1);
+//        System.out.println(substring);
+//        String tableNameUpperCase = upperCase + substring;
+//        System.out.println(tableNameUpperCase);
+//        String className = String.format("%sService", tableNameUpperCase);
+//        System.out.println(className);
+//
+//        Map<String, Object> data = new HashMap<>();
+//        data.put("className", className);
+//
+//        CustomFreemarkerUtil.generate(toPath + className + ".java", data);
+        readConfigurationFileFromPomXml();
+    }
 
-        String tableName = "user";
-        String c = tableName.charAt(0) + "";
-        String upperCase = c.toUpperCase();
-        System.out.println(upperCase);
-        String substring = tableName.substring(1);
-        System.out.println(substring);
-        String tableNameUpperCase = upperCase + substring;
-        System.out.println(tableNameUpperCase);
-        String className = String.format("%sService", tableNameUpperCase);
-        System.out.println(className);
-
-        Map<String, Object> data = new HashMap<>();
-        data.put("className", className);
-
-        CustomFreemarkerUtil.generate(toPath + className + ".java", data);
+    private static String readConfigurationFileFromPomXml() throws DocumentException {
+        SAXReader saxReader = new SAXReader();
+        HashMap<String, String> map = new HashMap<>();
+        map.put("pom", "http://maven.apache.org/POM/4.0.0");
+        saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
+        Document document = saxReader.read(pomPath);
+        Node node = document.selectSingleNode("//pom:configurationFile");
+        String configurationFile = node.getText();
+        System.out.println("从 pom.xml 读取 mybatis-generator-maven-plugin 需要用到的 configurationFile=" + configurationFile);
+        return configurationFile;
     }
 }
